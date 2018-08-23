5e5fc904a620587369d975eba6a22241f8169958
huhan <947751771@qq.com>
2018-08-23 21:29:23 +0800

    新增分类

--- src/main/java/com/huhan/blog/web/admin/TypesController.java
+++ src/main/java/com/huhan/blog/web/admin/TypesController.java
@@ -1,5 +1,6 @@
 package com.huhan.blog.web.admin;
 
+import com.huhan.blog.po.Type;
 import com.huhan.blog.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,15 +27,41 @@ public class TypesController {
 
     /**
      * 分页查询
+     *
      * @param pageable
      * @param model
      * @return
      */
     @GetMapping("/types")
-    private String list(@PageableDefault(size = 10, sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable
+    public String list(@PageableDefault(size = 10, sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable
             , Model model) {
 
         model.addAttribute("page", typeService.listType(pageable));
         return "admin/types";
+    }
+
+    /**
+     *
+     * @return
+     */
+    @GetMapping("/types/input")
+    public String addTypesPre() {
+
+        return "admin/type-input";
+    }
+
+    /**
+     * 分类新增
+     * @param type
+     * @return
+     */
+    @PostMapping("/types")
+    public String addType(Type type) {
+        Type save = typeService.save(type);
+        if (save == null) {
+
+        }
+
+        return "redirect:/admin/types";
     }
 }
